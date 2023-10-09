124be41c7107c4cf4ae94fd185c7bad08068ebbd
Marcio Miranda <Marcio_lgm@outlook.com>
2023-10-09 10:10:29 -0100

    Inserindo EX06

---
+++ EX04 CONSTRUTORES/EX04 CONSTRUTORES/Conta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX04_CONSTRUTORES
+{
+    internal class Conta
+    {
+        //Metodos CONSTRUTORES
+        public Conta(int numero, double limite) 
+        {
+            this.Numero = numero;
+                 Limite = limite;
+        }
+        
+
+        //Propiedades
+        private double Saldo { get; set; }
+        public double Limite { get; private set; } //Limite de credito que o Banco disponibilisa a um cliente!
+        public int Numero { get; private set; }
+
+        //Metodos
+        public void Deposita(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public double ConsultaSaldoDisponivel()
+        {
+            return Saldo + Limite;
+        }
+
+        public double AdicionarLimite(double valor)
+        {
+            return Limite = valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            double saldoDisponivel = ConsultaSaldoDisponivel();
+            if (saldoDisponivel < valor)
+            {
+                Console.WriteLine(" Saldo e saque Indisponivel!");
+                return false;
+
+            }
+            else Saldo -= valor;
+            return true;
+
+        }
+    }
+}
+    
+
---
+++ EX04 CONSTRUTORES/EX04 CONSTRUTORES/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX04_CONSTRUTORES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Criando instancias da classe Conta
+            Conta conta = new Conta(123, 400);
+
+         
+            //conta.AdicionarLimite(1500);
+
+            conta.Deposita(1500);
+            bool sacar = conta.Sacar(300);
+
+            if (sacar)
+            {
+                Console.WriteLine(" Saque com SUCESSO!");
+                Console.WriteLine(" O Saldo atual de: " + conta.ConsultaSaldoDisponivel());
+                Console.WriteLine(" O numero da conta e:" + conta.Numero);
+                Console.WriteLine(" O Limite e de padrao: " + conta.Limite);
+               // Console.WriteLine(" O Limite e Adicionado: " + conta.AdicionarLimite());
+                //Console.WriteLine(" O Saldo e de: " + conta.Saldo);
+                //Console.WriteLine(" O Limite e de: " + conta.Limite);
+                //Console.WriteLine(sacar);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
